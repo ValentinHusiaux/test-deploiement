df7c8952bb3f8122fbf9e615d9c47219b5bf9dd2
ValentinHusiaux <Valentin.husiaux@livecampus.tech>
2022-09-27 12:26:24 +0200

    testSoustraction

--- function.js
+++ function.js
@@ -14,12 +14,18 @@ class calculatrice {
         result = a + b 
         return result
     }
+
+    soustraction(a,b){
+        let result
+        result = a - b 
+        return result
+    }
 }
 
 function testAdd(a ,b) {
     let testResult = a + b 
     let calcule = new calculatrice(1,5)
-    let result = calcule.add(1,4)
+    let result = calcule.add(1,5)
 
     if (result === testResult) {
     return console.log("c bon")
@@ -30,3 +36,19 @@ function testAdd(a ,b) {
 }
 
 testAdd(1,5)
+
+
+function testSoustraction(a ,b) {
+    let testResult = a - b 
+    let calcule = new calculatrice(1,5)
+    let result = calcule.soustraction(1,5)
+
+    if (result === testResult) {
+    return console.log("c bon")
+    } else  {
+        return console.log("pas bon")
+    }
+    
+}
+
+ testSoustraction(1,5)
