54697a0092a17ffd9c181eb5457d054757911df6
ValentinHusiaux <Valentin.husiaux@livecampus.tech>
2022-09-27 12:28:17 +0200

    multi et div

--- function.js
+++ function.js
@@ -20,6 +20,19 @@ class calculatrice {
         result = a - b 
         return result
     }
+
+    div(a,b) {
+        let result
+        result = a / b 
+        return result
+    }
+
+    multi(a,b)
+    {
+        let result
+        result = a * b 
+        return result
+    }
 }
 
 function testAdd(a ,b) {
@@ -52,3 +65,34 @@ function testSoustraction(a ,b) {
 }
 
  testSoustraction(1,5)
+
+
+ function testDiv(a ,b) {
+    let testResult = a / b 
+    let calcule = new calculatrice(1,5)
+    let result = calcule.div(1,5)
+
+    if (result === testResult) {
+    return console.log("c bon")
+    } else  {
+        return console.log("pas bon")
+    }
+    
+}
+
+testDiv(1,5)
+
+function testMulti(a ,b) {
+    let testResult = a * b 
+    let calcule = new calculatrice(1,5)
+    let result = calcule.multi(1,5)
+
+    if (result === testResult) {
+    return console.log("c bon")
+    } else  {
+        return console.log("pas bon")
+    }
+    
+}
+
+testMulti(1,5)
