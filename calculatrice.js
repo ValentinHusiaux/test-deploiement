e7ae9fda1167b9091c8129d90f1bec9ef46c639f
ValentinHusiaux <Valentin.husiaux@livecampus.tech>
2022-09-27 11:45:47 +0200

    v1

---
+++ calculatrice.js
@@ -0,0 +1,10 @@
+
+export class calcul {
+  constructor ()
+
+  Add(a,b) {
+    return a+ b;
+  }
+
+  
+}
---
+++ test.js
@@ -0,0 +1,8 @@
+import * as calculatrice from 'calculatrice'
+
+
+let a = 3
+let b = 2
+
+
+const testAdd = calculatrice.add
